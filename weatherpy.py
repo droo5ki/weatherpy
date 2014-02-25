4d3e6c36f3da7486e2477000c3034be4b394d587
etch <etch.himself@gmail.com>
2014-02-24 22:13:28 -0800

    Get temp and basic output works

--- weatherpy.py
+++ weatherpy.py
@@ -1,25 +1,22 @@
+# -*- coding: utf-8 -*-
+
 import sys
 import requests
 
-def getResponseFromWunderground(zipcode):
+def getCityName(zipcode):
   ''' need to figure out how the '''
   ''' docsting stuff works '''
   
   url = 'http://api.wunderground.com/api/0b0139e2a0f25fb7/geolookup/q/%s.json' % zipcode
-
-  # construct get request
+  response = requests.get(url)
   
-  return requests.get(url)
-
-def getCityName(response):
- 
   return response.json()['location']['city']
 
-def getCurrentTemperature(response):
-  print response.json()
-  print 
-  print ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-  return "Current temperature is %s" % response.json()['current_observation']['temp_f']
+def getCurrentTemp(zipcode):
+  
+    url = 'http://api.wunderground.com/api/0b0139e2a0f25fb7/conditions/q/%s.json' % zipcode
+    response = requests.get(url)
+    return response.json()['current_observation']['temp_f']
 
 if __name__ == '__main__':
   
@@ -28,7 +25,7 @@ if __name__ == '__main__':
      print 'Too few args'
      exit()
   else:
-    response = getResponseFromWunderground(zipcode)
-    print getCityName(response)
-    print getCurrentTemperature(response)
-    
+    city = getCityName(zipcode)
+    curr_temp_f = getCurrentTemp(zipcode) 
+  d = '\xb0'
+  print "The current temperature is %s F in %s." % (curr_temp_f,city)
